edb17c3f55e6919bd4bad83296bc47e26dd9d306
yoenheep <yoen3212@naver.com>
2025-07-23 11:01:15 +0900

    배포 주소 변경

--- app/src/main/java/com/walkee/walkee/Common/PageInfo.java
+++ app/src/main/java/com/walkee/walkee/Common/PageInfo.java
@@ -3,7 +3,7 @@ package com.walkee.walkee.Common;
 // 웹뷰띄울 url주소 넣는곳
 public class PageInfo {
     // 여기 주소 바꿔주면 됨
-    public static final String INDEX_PAGE = "http://walkeeteam.s3-website.ap-northeast-2.amazonaws.com/";
+    public static final String INDEX_PAGE = "https://d1i3d05ur40jbn.cloudfront.net";
 
 }
 
